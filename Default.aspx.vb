0aa173fbb469199c1d90b3b1907c33b86c294fe6
mallemann <Martin.allemann@bluewin.ch>
2020-01-15 09:52:26 +0100

    Update Default.aspx.vb

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -3,6 +3,6 @@ Partial Class _Default
     Inherits System.Web.UI.Page
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Label1.Text = "hello world new"
+        Label1.Text = "hello world new 1"
     End Sub
 End Class
